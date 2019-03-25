393c877b0cb6beb7629a02acd03ad57c3d5faaf0
zied_sbabti <mohamedzied.sbabti@esprit.tn>
2019-03-25 13:47:05 +0100

    search finished + starting the share functionnality

--- web/js/evenements/eventBase.js
+++ web/js/evenements/eventBase.js
@@ -14,17 +14,50 @@ function search(href) {
         console.log(jsonArray);
     });
 }
-$("#search").keyup(function () {
+function fill(imageRef) {
     var filtredArray = [];
-    var searchVal = $(this).val();
+    var searchVal = $("#search").val();
+    var ulItem = $('#searchList ul');
+    ulItem.html("");
     filtredArray = jsonArray.filter(function (e) {
-        //return /^test/.test(e.titre);
         return e.titre.substring(0, searchVal.length) == searchVal;
     }).slice(0, 5);
     console.log(filtredArray);
-});
+    filtredArray.forEach(function (i) {
+        var date;
+        //@ts-ignore
+        date = new Date(i.date.timestamp * 1000);
+        var liItem = $('<li class="d-flex p-2 align-items-center border-bottom"></li>');
+        var divItem1 = $('<div class="col-md-2"> <img src="' + imageRef + i.urlImage + '" class="img-fluid"> </div>');
+        var divItem2 = $('<div class="col-md-10"></div>');
+        var divItem3 = $('<div class="d-flex justify-content-between"></div>');
+        var spanItem = $('<span> ' + i.titre + '</span> ');
+        var small1Item = $('<small class="text-muted">' + date.toLocaleDateString() + '</small>');
+        var small2Item = $('<small class="text-muted"><i class="fas fa-map-marker-alt"></i> ' + i.adresse + '</small>');
+        divItem3.append(spanItem, small1Item);
+        divItem2.append(divItem3, small2Item);
+        liItem.append(divItem1, divItem2);
+        ulItem.append(liItem);
+        liItem.hover(changeBack);
+    });
+}
+;
+function changeBack() {
+    $("#searchList li").hover(function () {
+        $(this).css('background-color', '#ECECEC');
+    }, function () {
+        $(this).css('background-color', 'white');
+    });
+}
 $(document).ready(function () {
     $('.closeJs').click(function () {
         $(this).parent().slideUp(500);
+    });
+    $("#search").click(function (e) {
+        e.stopPropagation();
+        $("#searchList").show();
+    });
+    $(document).click(function () {
+        $("#searchList").hide();
     });
 });
--- web/js/evenements/eventBase.ts
+++ web/js/evenements/eventBase.ts
@@ -24,19 +24,57 @@ function search(href : string){
     });
 }
 
-$("#search").keyup(function(){
+function fill (imageRef : string){
     let filtredArray=[];
-    let searchVal =<string> $(this).val();
+    let searchVal =<string> $("#search").val();
+    let ulItem = $('#searchList ul');
+    ulItem.html("");
+
     filtredArray = jsonArray.filter(function(e){
         return e.titre.substring(0,searchVal.length)==searchVal;
     }).slice(0,5);
     console.log(filtredArray);
-});
+    filtredArray.forEach((i)=>{
+        let date : Date;
+        //@ts-ignore
+        date = new Date(i.date.timestamp*1000);
+
+        let liItem = $('<li class="d-flex p-2 align-items-center border-bottom"></li>');
+        let divItem1 = $('<div class="col-md-2"> <img src="'+imageRef+i.urlImage+'" class="img-fluid"> </div>');
+        let divItem2 = $('<div class="col-md-10"></div>');
+        let divItem3 = $('<div class="d-flex justify-content-between"></div>');
+        let spanItem = $('<span> '+i.titre+'</span> ');
+        let small1Item = $('<small class="text-muted">'+date.toLocaleDateString()+'</small>');
+        let small2Item = $('<small class="text-muted"><i class="fas fa-map-marker-alt"></i> '+i.adresse+'</small>');
+
+        divItem3.append(spanItem, small1Item);
+        divItem2.append(divItem3,small2Item);
+        liItem.append(divItem1, divItem2);
+        ulItem.append(liItem);
+        liItem.hover(changeBack);
+    });
+};
+
+function changeBack (){    
+    $("#searchList li").hover(function(){
+        $(this).css('background-color' ,'#ECECEC');
+      },
+      function(){
+        $(this).css('background-color' ,'white');
+      })
+    }
 
 $(document).ready(function () {
     $('.closeJs').click(function(){
         $(this).parent().slideUp(500);
-    });
-    
+    });      
+      $("#search").click(function(e){
+        e.stopPropagation();
+        $("#searchList").show();
+      });
+      $(document).click(function(){
+        $("#searchList").hide();
+      });
+
 });
 
